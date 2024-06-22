0cfb0ed9719e643f903d960c939836dc8722e6b3
Santino <tinomaturo14@gmail.com>
2024-06-22 16:22:31 -0300

    subida de proyecto

---
+++ index.js
@@ -0,0 +1,48 @@
+// Aplicacion que nos muestre el clima de la ciudad que nosotros ingresemos
+let api_key = "22edd86fd3365d0107c62d1c4c41a096";
+// Referencia para el link con el que vamos a llamar a la API
+let urlApi = "https://api.openweathermap.org/data/2.5/weather";
+const difKelvin = 273.15;
+
+document.getElementById("botonBusqueda").addEventListener("click", () => {
+  // La ciuda va a ser igual al valor que se ingrese en el input
+  const ciudad = document.getElementById("ciudadEntrada").value;
+  // Si hay algo en ciudad
+  if (ciudad) {
+    fetchDatosClima(ciudad);
+  }
+});
+
+// Creamos una funcion para invocar a la API
+function fetchDatosClima(ciudad) {
+  // Pedimos los datos de la API
+  fetch(`${urlApi}?q=${ciudad}&appid=${api_key}`)
+    // Usamos .then() para retornar la respuesta exitosa del fetch
+    // Convertimos el JSON que nos llega en un objeto javascript para poder trabajarlo
+    .then((response) => response.json())
+    // Retornamos ahora si la respuesta que va a ser el JSON transformado en objeto javascript y vamos a usar una funcion para mostrar los datos del clima con la respuesta del fetch como argumento
+    .then((response) => mostrarDatosClima(response));
+}
+
+function mostrarDatosClima(response) {
+  console.log(response);
+  // Seleccionamos el div donde van a ir los datos del clima
+  const divDatosClima = document.getElementById("datosClima");
+  divDatosClima.innerHTML = ""; // Vaciamos los datos para que se resetee
+  const ciudadNombre = response.name;
+  // Temperatura está dentro del objeto main y la clave temp
+  const temperatura = response.main.temp;
+  const descripcion = response.weather[0].description;
+  // Creamos un h2
+  const ciudadTitulo = document.createElement("h2");
+  ciudadTitulo.textContent = ciudadNombre;
+  const temperaturaInfo = document.createElement("p");
+  temperaturaInfo.textContent = `La temperatura es: ${Math.round(
+    temperatura - difKelvin
+  )}°C`;
+  const descripcionInfo = document.createElement("p");
+  descripcionInfo.textContent = `La descripcion meteorologica es de : ${descripcion}`;
+  divDatosClima.appendChild(ciudadTitulo);
+  divDatosClima.appendChild(temperaturaInfo);
+  divDatosClima.appendChild(descripcionInfo);
+}
